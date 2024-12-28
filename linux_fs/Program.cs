2a2fb0a467092b094daccf36d82bed1ba4970b15
taworld-star <sustrisimamora0413@gmail.com>
2024-12-28 19:21:47 +0700

    Add static and dictionary

--- linux_fs/Program.cs
+++ linux_fs/Program.cs
@@ -1,5 +1,23 @@
 ﻿class Program {
+
+    static FileSystem fs = new FileSystem();
     public static void Main() {
 
+        // Dictionary untuk memetakan perintah string ke fungsi yang sesuai
+            Dictionary<string, Action<string[]>> commands = new Dictionary<string, Action<string[]>>
+            {
+                { "touch", TouchCommand },
+                { "mkdir", MkdirCommand },
+                { "cd", CdCommand },
+                { "ls", LsCommand },
+                { "rm", RmCommand },
+                { "mv", MvCommand },
+                { "cp", CpCommand },
+                { "pwd", PwdCommand },
+                 { "locate", LocateCommand },
+                { "exit", ExitCommand }
+            };
+        
+        
     }
 }
