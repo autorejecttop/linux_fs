456e3173684801d6a931f3a58f48975e47e2e3a7
habuu <agunghabuu@gmail.com>
2024-12-29 23:28:28 +0700

    add GetTree method for TreeSystem

--- linux_fs/TreeSystem.cs
+++ linux_fs/TreeSystem.cs
@@ -1,13 +1,22 @@
-// Komentar
 public class TreeSystem {
-    public String data;
+    public String name;
     public Type type;
     public List<TreeSystem> children;
 
-    public TreeSystem(String data, Type type) {
-        this.data = data;
+    public TreeSystem(String name, Type type) {
+        this.name = name;
         this.type = type;
         this.children = [];
+    }
+
+    public TreeSystem? GetTree(String data, Type type) {
+        TreeSystem? result = null;
+        foreach (TreeSystem tree in children) {
+            if (tree.name.Equals(data) && tree.type.Equals(type)) {
+                result = tree;
+            }
+        }
+        return result;
     }
 }
 
