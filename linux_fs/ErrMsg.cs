5e037d1d1967b86ff80629c1189c4f963366bd8f
habuu <agunghabuu@gmail.com>
2024-12-30 15:22:05 +0700

    better error visuals

--- linux_fs/ErrMsg.cs
+++ linux_fs/ErrMsg.cs
@@ -1,11 +1,22 @@
 class ERRMSG {
+    public static void INV_COMMAND(String arg) {
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine($" ERR: \"{arg}\" <- INVALID COMMAND ");
+        Console.ResetColor();
+    }
+
     public static void FILE_NOT_FOUND(String arg) {
-        Console.WriteLine($"{arg}\" <- FILE NOT FOUND ");
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine($" ERR: \"{arg}\" <- FILE NOT FOUND ");
+        Console.ResetColor();
     }
 
     public static void DIR_NOT_FOUND(String arg) {
         Console.BackgroundColor = ConsoleColor.Red;
         Console.ForegroundColor = ConsoleColor.White;
-        Console.WriteLine($"{arg}\" <- DIRECTORY NOT FOUND ");
+        Console.WriteLine($" ERR: \"{arg}\" <- DIRECTORY NOT FOUND ");
+        Console.ResetColor();
     }
 }
