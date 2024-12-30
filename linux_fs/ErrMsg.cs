7581430fc5ebf6106a6ebbe912568be2d3f38297
habuu <agunghabuu@gmail.com>
2024-12-30 22:43:37 +0700

    add error message

--- linux_fs/ErrMsg.cs
+++ linux_fs/ErrMsg.cs
@@ -33,4 +33,12 @@ class ERRMSG {
         Console.WriteLine($" ERR: \"{arg}\" <- DIRECTORY NOT FOUND ");
         Console.ResetColor();
     }
+
+    public static void FILE_DIR_NOT_FOUND(String arg) {
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine($" ERR: \"{arg}\" <- FILE/DIRECTORY NOT FOUND ");
+        Console.ResetColor();
+    }
+
 }
