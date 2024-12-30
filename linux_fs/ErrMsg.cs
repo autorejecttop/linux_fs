a6aaf90d159046ccb6d391524868793c50944e7a
habuu <agunghabuu@gmail.com>
2024-12-30 12:39:17 +0700

    better error message

--- linux_fs/ErrMsg.cs
+++ linux_fs/ErrMsg.cs
@@ -1,9 +1,9 @@
 class ERRMSG {
-    public static void FILE_NOT_FOUND() {
-        Console.WriteLine("\n== FILE NOT FOUND ==\n");
+    public static void FILE_NOT_FOUND(String arg) {
+        Console.WriteLine($"{arg}: FILE NOT FOUND");
     }
 
-    public static void FOLDER_NOT_FOUND() {
-        Console.WriteLine("\n== FOLDER NOT FOUND ==\n");
+    public static void FOLDER_NOT_FOUND(String arg) {
+        Console.WriteLine($"{arg}: FOLDER NOT FOUND");
     }
 }
